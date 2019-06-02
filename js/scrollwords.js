c6e67ab7327be3d4fae912467d6dc37839b86913
Vishnu Vijai <vishnuvijai@gmail.com>
2019-06-02 12:21:21 +0530

    Finished animation of svg, Working on few-words Image

--- js/scrollwords.js
+++ js/scrollwords.js
@@ -1,21 +1,27 @@
 $(document).ready(function(){
-    stopAnimation(".pie svg:nth-child(2) path","none");
-    $("#panel").stop();
+let selectorOne = "section.words > .words-container .row:last-child .words-svg-cols:first-child > .pie li svg:last-child path";
+let selectorTwo = "section.words > .words-container .row:last-child .words-svg-cols:nth-child(2) > .pie li svg:last-child path";
+let selectorThree = "section.words > .words-container .row:last-child .words-svg-cols:nth-child(3) > .pie li svg:last-child path";
+let selectorFour = "section.words > .words-container .row:last-child .words-svg-cols:nth-child(4) > .pie li svg:last-child path";
     $(document).scroll(function(){
         let val = $(window).scrollTop();
         
-        if (val> 3756 ) {
-            stopAnimation(".pie svg:nth-child(2) path","load 2s");
-        } else{
+        if (val> 3500 ) {
+            startAnimation(selectorOne,"load 2s","628");
+            startAnimation(selectorTwo,"load 2s","610");
+            startAnimation(selectorThree,"load 2s","628");
+            startAnimation(selectorFour,"load 2s","610");
             
-        }
-            
-        
+        } 
     });
 });
 
-function stopAnimation(element,action)
+function startAnimation(element,action,value)
 {
+    
+    if (action != "none") {
+        $(element).attr("stroke-dashoffset",value);
+    }
     $(element).css("-webkit-animation", action);
     $(element).css("-moz-animation", action);
     $(element).css("-ms-animation", action);
